1bc82e556baca3f8334ffd9679fa8c6a19e6cde5
Robert Yang <yang.robert.w@gmail.com>
2015-10-27 19:29:12 -0400

    week08: intro to Unity and terrain... week09a: intro to code, making simple score-based text games

---
+++ week09/Assets/scripts/ReflexGame.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI; // VERY IMPORTANT, YOU NEED THIS TO TALK TO UI COMPONENTS
+
+public class ReflexGame : MonoBehaviour {
+	
+	float timeHeldDown = 0.0f;
+
+	// "public" keyword when declaring a class member var = exposes to Unity Editor inspector
+	public Text myTextComponent;
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey (KeyCode.Space) ) {
+			// Time.deltaTime is the duration of the frame in seconds
+			timeHeldDown += Time.deltaTime;
+			myTextComponent.text = "TIME HELD DOWN: " + timeHeldDown.ToString();
+			Debug.Log ( timeHeldDown.ToString() );
+		}
+	}
+}
---
+++ week09/Assets/scripts/TapGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapGame : MonoBehaviour {
+
+	// the player's current score; goes from 0 to 100
+	int score = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKeyDown (KeyCode.Space) ) {
+			score += 1; // add 1 point to score
+			Debug.Log ( "CURRENT SCORE: " + score.ToString() );
+		}
+
+	}
+}
