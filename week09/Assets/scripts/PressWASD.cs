91d70b8594d22cfc495d9b23562a63374985d4aa
Robert Yang <yang.robert.w@gmail.com>
2015-10-28 13:37:35 -0400

    week09b: PressWASD game, begin text adventure

---
+++ week09/Assets/scripts/PressWASD.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PressWASD : MonoBehaviour {
+
+	// private variables are accessible ONLY in this class
+	private Text myTextComponent;
+
+	// Use this for initialization
+	void Start () {
+		// tell Unity to look for a "Text" component next to this script
+		myTextComponent = GetComponent<Text>();
+		myTextComponent.text = "Hey this works!";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// this will fire when the player RELEASES the W key
+		if (Input.GetKeyUp (KeyCode.W)) {
+			myTextComponent.text = "You pressed W! Good job, champ!";
+		}
+
+		// this will happen when player holds down A, S, and D
+		if ( Input.GetKey (KeyCode.A) && Input.GetKey (KeyCode.S) && Input.GetKey(KeyCode.D)) {
+			// "+=" adds on top of the existing value
+			myTextComponent.text += "\n\nWow your power is astounding!"; // "\n" is a line break
+		}
+	}
+}
