4e9931262c29772278b65b3958e2b420bfdacf6d
Robert Yang <yang.robert.w@gmail.com>
2015-11-02 15:49:23 -0500

    week10a: intro to vectors, moving cubes and spheres around

---
+++ week10/Assets/scripts/CubeMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeMove : MonoBehaviour {
+
+	// public variables in Unity "expose it to the inspector", which will help us TUNE it
+	public float moveSpeed = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+		// up arrow moves us 0.1f on Z axis
+		if ( Input.GetKey( KeyCode.UpArrow ) ) {
+			transform.position = transform.position + new Vector3( 0f, 0f, moveSpeed);
+		}
+
+		// left arrow moves us -0.1f on X axis
+		if ( Input.GetKey( KeyCode.LeftArrow ) ) {
+			transform.position = transform.position + new Vector3( -moveSpeed, 0f, 0f);
+		}
+
+		// right arrow move us 0.1f on X axis
+		if ( Input.GetKey( KeyCode.RightArrow ) ) {
+			transform.position = transform.position + new Vector3( moveSpeed, 0f, 0f);
+		}
+
+		// down arrow move us -0.1f on Z axis
+		if ( Input.GetKey( KeyCode.DownArrow ) ) {
+			transform.position = transform.position + new Vector3( 0f, 0f, -moveSpeed);
+		}
+
+	}
+}
---
+++ week10/Assets/scripts/SphereMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereMove : MonoBehaviour {
+
+	public float moveSpeed = 0.1f;
+	public float turnSpeed = 5f;
+
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey (KeyCode.W) ) {
+			// transform.forward is the "forward" direction of the object
+			transform.position += transform.forward * moveSpeed;
+			// transform.position += Vector3.forward; // Vector3.forward is ALWAY "south"
+		}
+
+		if ( Input.GetKey (KeyCode.A) ) {
+			transform.Rotate ( 0f, -turnSpeed, 0f );
+		}
+
+		// if I hold down D, rotate 15 degrees to my right
+		if ( Input.GetKey (KeyCode.D) ) {
+			transform.Rotate ( 0f, turnSpeed, 0f );
+		}
+	}
+}
