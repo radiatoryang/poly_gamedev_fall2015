06104eedb2ef23106b3650de79da74f5dcb1a2db
Robert Yang <yang.robert.w@gmail.com>
2015-11-04 13:52:01 -0500

    week10b: capsuleMove.cs and mouse input, physics rigidbody demo

---
+++ week10/Assets/scripts/CapsuleMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CapsuleMove : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetKey ( KeyCode.W ) ) {
+			transform.position += transform.forward * 0.25f;
+		}
+
+		float mouseX = Input.GetAxis ("Mouse X");
+		transform.Rotate ( 0f, mouseX * 45f, 0f );
+	}
+
+}
