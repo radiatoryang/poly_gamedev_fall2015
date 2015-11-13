1fe925968d74a46a78b5fb9f763bc447078be90f
Robert Yang <yang.robert.w@gmail.com>
2015-11-13 17:15:30 -0500

    week11: RigidbodyMove.cs demo, making a controllable player object with rigidbody forces

---
+++ week11/Assets/scripts/RigidbodyMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigidbodyMove : MonoBehaviour {
+
+	Rigidbody rbody;
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody>();
+	}
+	
+	// FixedUpdate is called on the same framerate that PHYSICS runs
+	void FixedUpdate () {
+		// forward / backward movement
+		float vertical = Input.GetAxis ("Vertical");
+		// add gravity at the end to make it less floaty
+		rbody.velocity = transform.forward * vertical * 25f + Physics.gravity;
+
+		// turning left / right does NOT use physics
+		float horizontal = Input.GetAxis ("Horizontal");
+		transform.Rotate ( 0f, horizontal * Time.deltaTime * 180f, 0f );
+	}
+}
---
+++ week11/Assets/scripts/VectorMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VectorMove : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		// "Horizontal" is Left/Right, A/D
+		float horizontal = Input.GetAxis ("Horizontal");
+		// rotate the player
+		const float turnRate = 45f;
+		transform.Rotate (0f, horizontal * turnRate * Time.deltaTime, 0f );
+
+		// "Vertical" is Up/Down, W/S
+		float vertical = Input.GetAxis ("Vertical");
+		// move the player
+		const float moveSpeed = 5f;
+		transform.position += transform.forward * vertical * Time.deltaTime * moveSpeed;
+
+	}
+}
