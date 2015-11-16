328a00e5877aab465391b11ece3c5b052f17f415
Robert Yang <yang.robert.w@gmail.com>
2015-11-16 15:49:26 -0500

    week12: instantiate, game manager, changing scenes, restart button

---
+++ week12/Assets/scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// to clone something, you first need an original thing to clone
+	public GameObject thingToClone; // assign in Inspector
+
+	// Use this for initialization
+	void Start () {
+		// if NIGHTMARE MODE IS ENABLED, turn the cube red
+		if ( MenuScreen.useNightmareMode == true ) {
+			thingToClone.GetComponent<Renderer>().material.color = new Color(1f, 0f, 0f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// our main mechanic: MAKE CUBES!!!
+		if ( Input.GetKeyDown (KeyCode.Space) ) {
+			Instantiate ( thingToClone, new Vector3( Random.Range (-5f, 5f), 0f, Random.Range (-5f, 5f) ), Quaternion.Euler(0f, 45f, 0f) );
+		}
+
+		// simple restart
+		if ( Input.GetKeyDown (KeyCode.R) ) {
+			Application.LoadLevel ( 0 ); // load scene number 0 (go to File > Build Settings in Unity)
+		}
+	}
+}
---
+++ week12/Assets/scripts/MenuScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScreen : MonoBehaviour {
+
+	// "PUBLIC STATIC" makes data persist across scene changes + globally accessable by any script
+	public static bool useNightmareMode = false;
+
+	// Use this for initialization
+	void Start () {
+		// manually reset nightmare mode
+		useNightmareMode = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// options mode toggle
+		if ( Input.GetKeyDown (KeyCode.N) ) {
+			useNightmareMode = !useNightmareMode; // toggle the bool
+		}
+
+		// press space to start game
+		if ( Input.GetKeyDown (KeyCode.Space) ) {
+			Application.LoadLevel( "instantiateDemo" ); // load the game
+		}
+	}
+}
